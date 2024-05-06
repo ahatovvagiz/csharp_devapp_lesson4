b3491931ec487fdab0ab4bf6923e9044ae07e647
vagiz <ahatovvagiz@gmail.com>
2024-05-06 10:01:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> Numbers = new List<int>
+        { 1, 2, 3, 4, 5, 6, 7 };
+        int target = 10;
+        Numbers.Sort();   
+             
+        for (int i = 0; i < Numbers.Count - 2; i++)
+        {
+            int left = i + 1;
+            int right = Numbers.Count - 1;
+
+            while (left < right)
+            {
+                int currentSum = Numbers[i] + Numbers[left] + Numbers[right];
+
+                if (currentSum == target)
+                {
+                    Console.WriteLine($"Найдены три числа: {Numbers[i]}, {Numbers[left]}, {Numbers[right]}");
+                    return;
+                }
+                else if (currentSum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+
+    }
+}
